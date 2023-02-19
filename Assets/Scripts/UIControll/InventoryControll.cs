7eb571ec25aab8b5f656b37d3140ac650d86f67e
Sherry <sherrylia0hy@gmail.com>
2023-02-19 11:15:24 -0600

    Add Inventory Window
    Add Inventory window. Can be toggle with key "i"

---
+++ Assets/Scripts/UIControll/InventoryControll.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryControll : MonoBehaviour
+{
+
+
+    public GameObject Inventory;
+    public bool inventoryIsClosed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventoryIsClosed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("i"))
+        {
+            if (inventoryIsClosed == true)
+            {
+                Inventory.SetActive(true);
+                inventoryIsClosed = false;
+            }
+            else
+            {
+                Inventory.SetActive(false);
+                inventoryIsClosed = true;
+            }
+        }
+    }
+
+}
