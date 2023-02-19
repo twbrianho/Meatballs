16a11799935520a9c8490a867f537b5a080a0628
stuts123 <79263707+stuts123@users.noreply.github.com>
2023-02-18 20:38:03 -0600

    Movement
    We can now move! sort of

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -13,6 +13,14 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        var rb = GetComponent<Rigidbody>();
+        if (Input.GetKey(KeyCode.A))
+            rb.AddForce(Vector3.left);
+        if (Input.GetKey(KeyCode.D))
+            rb.AddForce(Vector3.right);
+        if (Input.GetKey(KeyCode.W))
+            rb.AddForce(Vector3.up);
+        if (Input.GetKey(KeyCode.S))
+            rb.AddForce(Vector3.down);
     }
 }
